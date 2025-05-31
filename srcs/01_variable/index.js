4d2574bc3100122fc522a8b912fda5058010f98f
lockdoor <lockdoor@gmail.com>
2025-05-31 13:11:17 +0700

    first

--- srcs/01_variable/index.js
+++ srcs/01_variable/index.js
@@ -1,12 +1,17 @@
 // define a constant [PI] with the value 3.14 (cannot be reassigned)
+const PI = 3.14;
 
 // define a mutable block-scoped variable [answer] with the value 42
+let answer = 43;
 
 // define a mutable function-scoped variable [count] with the value 0
+var count = 0;
 
 // define a constant [SchoolOfCode] with the value '42Bangkok'
+const SchoolOfCode = '42Bangkok';
 
 // log all variables to the console ordered by declaration
+console.log(PI, answer, count, SchoolOfCode);
 
 /* do not change the code below #####################################*/
 module.exports = {
